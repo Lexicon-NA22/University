7ac5574c5954dfa5e983873b9e4e1690138bb881
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2022-02-09 11:27:10 +0100

    Add Context

---
+++ University.Data/Data/UniversityContext.cs
@@ -0,0 +1,21 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using University.Entities;
+
+namespace University.Data.Data
+{
+    public class UniversityContext : DbContext
+    {
+        public DbSet<Student> Student { get; set; }
+
+        public UniversityContext (DbContextOptions<UniversityContext> options)
+            : base(options)
+        {
+        }
+
+    }
+}
--- University.Web/Program.cs
+++ University.Web/Program.cs
@@ -1,4 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using University.Data.Data;
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<UniversityContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("UniversityContext")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
