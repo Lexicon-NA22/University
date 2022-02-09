80c5fd5cb72c6a467c13d9d52a911b32276806c2
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2022-02-09 11:54:23 +0100

    1:M Convention 1

---
+++ University.Data/Migrations/20220209105016_Enrollment.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace University.Data.Migrations
+{
+    public partial class Enrollment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Enrollment",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Grade = table.Column<int>(type: "int", nullable: false),
+                    StudentId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Enrollment", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Enrollment_Student_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Student",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Enrollment_StudentId",
+                table: "Enrollment",
+                column: "StudentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Enrollment");
+        }
+    }
+}
--- University.Entities/Enrollment.cs
+++ University.Entities/Enrollment.cs
@@ -4,11 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+#nullable disable
+
 namespace University.Entities
 {
     public class Enrollment
     {
         public int Id { get; set; }
         public int Grade { get; set; }
+
+        //Convention 1 - En student kan finnas på många olika Enrollments i tabellen
+        //               Kommer generera ett nullable int foreign key som ett shadow property
+        public Student Student { get; set; }
     }
 }
