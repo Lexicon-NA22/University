ab7bf0503a8d1360efad9fc7b7e8999e9878abb6
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2022-02-10 15:55:49 +0100

    ValidationResourses

--- University.Web/Validations/CheckStreetNrAttribute.cs
+++ University.Web/Validations/CheckStreetNrAttribute.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System.ComponentModel.DataAnnotations;
+using University.Web.Resourses;
 
 namespace University.Web.Validations
 {
@@ -10,12 +11,14 @@ namespace University.Web.Validations
         public CheckStreetNrAttribute(int max)
         {
             this.max = max;
+            ErrorMessageResourceType = typeof(ValidationResourses);
+            ErrorMessageResourceName = "StreetNr";
         }
 
         public void AddValidation(ClientModelValidationContext context)
         {
             context.Attributes.Add("data-val", "true");
-            context.Attributes.Add("data-val-streetnr", "Felmedellande från servern");
+            context.Attributes.Add("data-val-streetnr", ErrorMessageString);
             context.Attributes.Add("data-val-streetnr-max", $"{max}");
         }
 
