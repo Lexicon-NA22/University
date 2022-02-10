70bddd2c3c33dc10a322330b4e78481cd28ba315
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2022-02-10 15:40:23 +0100

    Client-Side validation

--- University.Web/Validations/CheckStreetNrAttribute.cs
+++ University.Web/Validations/CheckStreetNrAttribute.cs
@@ -1,14 +1,22 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
 
 namespace University.Web.Validations
 {
-    public class CheckStreetNrAttribute : ValidationAttribute
+    public class CheckStreetNrAttribute : ValidationAttribute, IClientModelValidator
     {
         private readonly int max;
 
         public CheckStreetNrAttribute(int max)
         {
             this.max = max;
+        }
+
+        public void AddValidation(ClientModelValidationContext context)
+        {
+            context.Attributes.Add("data-val", "true");
+            context.Attributes.Add("data-val-streetnr", "Felmedellande från servern");
+            context.Attributes.Add("data-val-streetnr-max", $"{max}");
         }
 
         public override bool IsValid(object? value)
