20a6fb234778cd4937e40d34d8b870c787b00153
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2022-02-11 13:08:20 +0100

    NullrefferensFilter

---
+++ University.Web/Filters/NullrefferensFilter.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace University.Web.Filters
+{
+    public class NullRefferenseFilter : ExceptionFilterAttribute
+    {
+
+        public override void OnException(ExceptionContext context)
+        {
+            if (context.Exception is NullReferenceException)
+            {
+                context.Result = new StatusCodeResult(500);
+            }
+        }
+    }
+}
